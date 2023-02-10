629970812ac1c78d7256b58be59a90c2900d84a5
gputnam <gputnam@icarusgpvm03.fnal.gov>
2023-02-10 13:54:20 -0600

    Various updated from ICARUS deployed instance.

--- minargon/static/js/minargon/alert.js
+++ minargon/static/js/minargon/alert.js
@@ -1,8 +1,8 @@
-export function throw_alert(text, divID) {
+export function throw_alert(text, divID, showIdler) {
   $(".alert-holder").append(
     `<div class="alert alert-primary alert-dismissible fade show" id="` + divID + `">` +
      `<div class="inline">` + text + `</div>` +
-    `<div class="hollowLoader inline"><div class="largeBox"></div><div class="smallBox"></div></div>` +
+    (showIdler ? `<div class="hollowLoader inline"><div class="largeBox"></div><div class="smallBox"></div></div>` : "") +
     `<button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria=hidden="true">&times;</span>
     </button>
