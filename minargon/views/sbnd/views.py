8098a1fb25291571d03194f5481625b3bd91395b
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2023-09-11 01:17:19 -0500

    read in multiple indices from elasticsearch, cleaning up plots + adding more info to page

--- minargon/views/sbnd/views.py
+++ minargon/views/sbnd/views.py
@@ -193,24 +193,21 @@ def DAB_Impedence_Ground_Monitor():
 @app.route('/Slow_Control_Alarms')
 def es_alarms():
     database = "sbnd_alarm_logger"
-    topic = "sbnd_alarms_state_2023-06-01"
     source_cols = [ "message_time", "time", "value", "message", "severity", "config" ]
 
-    es = elasticsearch_api.make_connection(database)
-    alarm_hits = elasticsearch_api.get_alarm_data(es, topic)
+    alarm_hits, extra_render_args = elasticsearch_api.get_alarm_data(database)
     alarms, component_hierarchy = elasticsearch_api.prep_alarms(alarm_hits, source_cols)
 
-    # print(alarms)
-    print(component_hierarchy)
-
-    alarms_pretty = (
-        "<br>".join(
-            jsonify(alarms).get_data(as_text=True).replace("\n", "<br>").split("<br>")[:50]
-        )
-    )
+    # alarms_pretty = (
+    #     "<br>".join(
+    #         jsonify(alarms).get_data(as_text=True).replace("\n", "<br>").split("<br>")[:50]
+    #     )
+    # )
     render_args = {
-        "alarms" : alarms, "components" : component_hierarchy, "alarms_pretty" : alarms_pretty
+        "alarms" : alarms, "components" : component_hierarchy
     }
+    render_args.update(extra_render_args)
+    print(render_args["current_time"], render_args["earliest_time"])
 
     return render_template('sbnd/es_alarms.html', **render_args)
 
